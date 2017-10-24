43715060ed6665b32bf3771d4c2d91029757951e
amagooda <ahmed.ezzat.gawad@gmail.com>
2017-10-24 12:38:55 -0400

    Add files via upload

---
+++ Part3/Part3_Keyboard.ino
@@ -0,0 +1,28 @@
+#include "pitches.h"
+
+const int threshold = 20;    // minimum reading of the sensors that generates a note
+
+// notes to play, corresponding to the 3 sensors:
+int notes[] = {
+  NOTE_B3, NOTE_C4
+};
+
+void setup() {
+}
+
+void loop() {
+    int sensorReading0 = analogRead(4);
+
+    // if the sensor is pressed hard enough:
+    if (sensorReading0 > threshold) {
+        // play the note corresponding to this sensor:
+        tone(8, NOTE_CS8, 10);
+      }
+      
+    int sensorReading1 = analogRead(1);
+
+    if (sensorReading1 > threshold) {
+        // play the note corresponding to this sensor:
+        tone(8, NOTE_B3, 10);
+      }
+}
