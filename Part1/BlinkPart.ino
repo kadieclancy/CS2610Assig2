223796c04d5a5742006558992367e7f51ea2d9f0
amagooda <ahmed.ezzat.gawad@gmail.com>
2017-10-24 12:39:31 -0400

    Add files via upload

---
+++ Part1/BlinkPart.ino
@@ -0,0 +1,14 @@
+const int ledPin1 = 1; 
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(ledPin1, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(ledPin1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(ledPin1, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
---
+++ Part2/ButtonPart.ino
@@ -0,0 +1,81 @@
+const int buttonPin = 13;     // the number of the pushbutton pin
+const int ledPin1 = 1;      // the number of the LED pin
+const int ledPin2 = 2;
+const int ledPin3 = 3;
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+int previousState = -1;
+int ledToLight = 0;
+ 
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+ 
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+ 
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // turn LED on:
+    if (previousState == buttonState)
+    {
+      
+      }
+    else
+    {
+      ledToLight = incrementLed(ledToLight);
+      lightALed(ledToLight);
+    }
+    //digitalWrite(ledPin, HIGH);
+  } else {
+  }
+  previousState = buttonState;
+}
+
+int incrementLed(int ledNumber_)
+{
+  ledNumber_ = ledNumber_ + 1;
+  if(ledNumber_ > 3)
+  {
+    ledNumber_ = 0;
+    }
+    return ledNumber_;
+}
+
+void lightALed(int ledNumber_)
+{
+  if(ledNumber_ == 0)
+  {
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+    
+    }
+  if(ledNumber_ == 1)
+  {
+    digitalWrite(ledPin1, HIGH);
+        digitalWrite(ledPin2, LOW);
+    digitalWrite(ledPin3, LOW);
+    }
+  if(ledNumber_ == 2)
+  {
+    digitalWrite(ledPin2, HIGH);
+        digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin3, LOW);
+    }
+  if(ledNumber_ == 3)
+  {
+    digitalWrite(ledPin3, HIGH);
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    }
+  
+  }
+
